2255814487e7840d67e6b549fb807b9cc0cd7992
Sergey S. Betke <sergey.s.betke@yandex.ru>
2016-05-23 20:33:01 +0300

    * выделил некоторые общие задачи в Makefile в отдельные файлы (#192)

---
+++ common.mk
@@ -0,0 +1,30 @@
+ifndef MAKE_COMMON
+MAKE_COMMON := TRUE
+
+MAKETARGETDIR      = /usr/bin/mkdir -p $(@D)
+
+ifeq ($(OS),Windows_NT)
+	PATHSEP          :=;
+else
+	PATHSEP          :=:
+endif
+
+ZIP                ?= zip \
+	-o \
+	-9
+TAR                ?= tar
+
+# $(call copyfile, to, from)
+define copyfile
+$1: $2
+	$$(MAKETARGETDIR)
+	cp $$< $$@
+endef
+
+# $(call copyfileto, todir, fromfile)
+copyfileto := $(call copyfile,$1/$(notdir $2),$2)
+
+# $(call copyfilefrom, tofile, fromdir)
+copyfilefrom = $(call copyfile,$1,$2/$(notdir $1))
+
+endif
