d64f3840a23ad5586e10e19484d7ba91c5febc54
sergey.s.betke <sergey.s.betke@yandex.ru>
2016-06-03 18:45:43 +0300

    - исправил часть ошибок в install.ps1 для выполнения сборки на appveyor (#189, #148)

--- install.cmd
+++ install.cmd
@@ -1,1 +1,1 @@
-powershell -NoProfile -ExecutionPolicy unrestricted -File install.ps1
+powershell -NoProfile -ExecutionPolicy unrestricted -File install.ps1 -GUI -ErrorAction Stop -Verbose
--- install.ps1
+++ install.ps1
@@ -1,22 +1,63 @@
-﻿Import-Module `
+﻿<# 
+.Synopsis 
+    Скрипт подготовки среды сборки и тестирования проекта GOST2-304
+.Description 
+    Скрипт подготовки среды сборки и тестирования проекта GOST2-304.
+    Используется в том числе и для подготовки среды на серверах appveyor.
+.Link 
+    https://github.com/Metrolog/Font.GOST2.304-81
+.Example 
+    .\install.ps1 -GUI;
+    Устанавливаем необходимые пакеты, в том числе - и графические среды.
+#> 
+[CmdletBinding(
+    SupportsShouldProcess = $true
+    , ConfirmImpact = 'Low'
+    , HelpUri = 'https://github.com/Metrolog/Font.GOST2.304-81'
+)]
+ 
+param (
+    # Ключ, определяющий необходимость установки визуальных средств.
+    # По умолчанию устанавливаются только средства для командной строки.
+    [Switch]
+    $GUI
+) 
+
+switch ( $env:PROCESSOR_ARCHITECTURE ) {
+    'amd64' { $ArchPath = 'x64'; }
+    'x86'   { $ArchPath = 'x86'; }
+    default { Write-Error 'Unsupported processor architecture.'}
+};
+$ToPath = @();
+
+Import-Module `
     -Name PackageManagement `
 ;
+
+Write-Verbose 'Preparing NuGet packages provider and sources...';
 Install-PackageProvider `
     -Name NuGet `
     -Force `
+    -OutVariable $null `
 ;
-Register-PackageSource `
-    -Name NuGet `
-    -ProviderName NuGet `
-    -Location 'http://packages.nuget.org/api/v2/' `
-    -Trusted `
-    -Force `
-;
+if ( (Get-PackageSource -ProviderName NuGet).count -eq 0 ) {
+    Register-PackageSource `
+        -Name NuGet `
+        -ProviderName NuGet `
+        -Location 'http://packages.nuget.org/api/v2/' `
+        -Trusted `
+        -Force `
+        -OutVariable $null `
+    ;
+};
+
+Write-Verbose 'Preparing Chocolatey packages provider and sources...';
 Install-PackageProvider `
     -Name Chocolatey `
     -Force `
+    -OutVariable $null `
 ;
-Import-PackageProvider `
+$null = Import-PackageProvider `
     -Name Chocolatey `
     -Force `
 ;
@@ -26,58 +67,63 @@ Register-PackageSource `
     -Location 'http://chocolatey.org/api/v2/' `
     -Trusted `
     -Force `
+    -OutVariable $null `
 ;
-Install-Package `
-    -Name 'NuGet.CommandLine' `
-    -ProviderName NuGet `
-    -Verbose `
-    -ForceBootstrap `
-    -Force `
-;
+$ToPath += "$env:ChocolateyPath\bin";
 
-
+Write-Verbose 'Preparing git...';
 Install-Package `
     -Name 'git' `
     -MinimumVersion '2.8' `
-    -Verbose `
     -Force `
+    -OutVariable $null `
 ;
 
+Write-Verbose 'Preparing cygwin...';
 Install-Package `
     -Name 'cygwin' `
     -Source chocolatey `
     -RequiredVersion '2.4.1' `
-    -Verbose `
     -ForceBootstrap `
     -Force `
+    -OutVariable $null `
 ;
 $env:CygWin = Get-ItemPropertyValue `
     -Path HKLM:\SOFTWARE\Cygwin\setup `
     -Name rootdir `
 ;
-[System.Environment]::SetEnvironmentVariable( 'CygWin', $env:CygWin, [System.EnvironmentVariableTarget]::Machine );
+if ($PSCmdLet.ShouldProcess('CygWin', 'Установить переменную окружения')) {
+    [System.Environment]::SetEnvironmentVariable( 'CygWin', $env:CygWin, [System.EnvironmentVariableTarget]::Machine );
+};
+$ToPath += "$env:CygWin\bin";
+
 Install-Package `
     -Name 'cyg-get' `
     -Source chocolatey `
-    -RequiredVersion '1.2.0' `
-    -Verbose `
+    -RequiredVersion '1.1.1' `
     -Force `
+    -OutVariable $null `
 ;
-Invoke-Expression "$env:ChocolateyPath\lib\cyg-get.$((Get-Package -Name 'cyg-get').Version)\tools\cyg-get.ps1 ttfautohint";
-Invoke-Expression "$env:ChocolateyPath\lib\cyg-get.$((Get-Package -Name 'cyg-get').Version)\tools\cyg-get.ps1 make";
-Invoke-Expression "$env:ChocolateyPath\lib\cyg-get.$((Get-Package -Name 'cyg-get').Version)\tools\cyg-get.ps1 zip";
+$CygGet = "$env:ChocolateyPath\lib\cyg-get.$((Get-Package -Name 'cyg-get').Version)\tools\cyg-get.ps1";
+if ($PSCmdLet.ShouldProcess('ttfautohint, make, zip', 'Установить пакет CygWin')) {
+    & $CygGet ttfautohint, make, zip;
+};
 
+Write-Verbose 'Preparing FontForge...';
 Install-Package `
     -Name 'fontforge' `
     -MinimumVersion '2015.08.24.20150930' `
-    -Verbose `
     -Force `
+    -OutVariable $null `
 ;
+$ToPath += "${env:ProgramFiles(x86)}\FontForgeBuilds\bin";
+
+Write-Verbose 'Preparing MikTeX...';
 Install-Package `
     -Name 'miktex' `
     -MinimumVersion '2.9' `
-    -Verbose `
     -Force `
+    -OutVariable $null `
 ;
 $MikTex = `
     Get-ChildItem `
@@ -86,53 +132,67 @@ $MikTex = `
     | Get-ItemPropertyValue `
         -Name InstallLocation `
 ;
-$MikTexBinPath = "$MikTex\miktex\bin\x64";
+$MikTexBinPath = "$MikTex\miktex\bin\$ArchPath";
+$ToPath += $MikTexBinPath;
 
+Write-Verbose 'Preparing WiX...';
 Install-Package `
     -Name 'WiX' `
     -MinimumVersion '4.0' `
     -Source NuGet `
-    -Verbose `
     -ForceBootstrap `
     -Force `
+    -OutVariable $null `
 ;
 $WixVersion = ( Get-Package -Name WiX -ProviderName NuGet ).Version;
 $env:WIXDIR = "$env:ProgramFiles\NuGet\Packages\WiX.$WixVersion\tools\";
-[System.Environment]::SetEnvironmentVariable( 'WIXDIR', $env:WIXDIR, [System.EnvironmentVariableTarget]::Machine );
+if ($PSCmdLet.ShouldProcess('WIXDIR', 'Установить переменную окружения')) {
+    [System.Environment]::SetEnvironmentVariable( 'WIXDIR', $env:WIXDIR, [System.EnvironmentVariableTarget]::Machine );
+};
+$ToPath += $env:WIXDIR;
 
-$env:Path = `
-    ( `
-        ( $env:Path -split ';' ) `
-        + (
-            "$env:ChocolateyPath\bin" `
-            , "$env:CygWin\bin" `
-            , "${env:ProgramFiles(x86)}\FontForgeBuilds\bin" `
-            , $MikTexBinPath `
-            , $env:WIXDIR `
-        ) `
-        | Sort-Object -Unique `
-    ) `
-    -join ';' `
-;
-[System.Environment]::SetEnvironmentVariable( 'PATH', $env:Path, [System.EnvironmentVariableTarget]::User );
-
+Write-Verbose 'Preparing ActivePerl...';
 Install-Package `
     -Name 'ActivePerl' `
-    -Verbose `
     -Force `
+    -OutVariable $null `
 ;
-& "ppm" install File::Copy::Recursive;
-& "$MikTexBinPath\mpm" --install=ctanify;
-& "ppm" install HTML::FormatText;
-& "$MikTexBinPath\mpm" --install=ctanupload;
 
+Write-Verbose 'Preparing ctanify and ctanupload TeX scripts...';
+if ($PSCmdLet.ShouldProcess('ctanify', 'Установить сценарий TeX и необходимые для него файлы')) {
+    & "ppm" install File::Copy::Recursive;
+    & "$MikTexBinPath\mpm" --install=ctanify;
+};
+if ($PSCmdLet.ShouldProcess('ctanupload', 'Установить сценарий TeX и необходимые для него файлы')) {
+    & "ppm" install HTML::FormatText;
+    & "$MikTexBinPath\mpm" --install=ctanupload;
+};
+
+Write-Verbose 'Preparing GitVersion...';
 Install-Package `
     -Name 'GitVersion.Portable' `
-    -Verbose `
     -Force `
+    -OutVariable $null `
 ;
-Install-Package `
-    -Name 'SourceTree' `
-    -Verbose `
-    -Force `
-;
+
+if ( $GUI ) {
+    Write-Verbose 'Preparing SourceTree...';
+    Install-Package `
+        -Name 'SourceTree' `
+        -Force `
+        -OutVariable $null `
+    ;
+};
+
+Write-Verbose 'Preparing PATH environment variable...';
+if ($PSCmdLet.ShouldProcess('PATH', 'Установить переменную окружения')) {
+    $env:Path = `
+        ( `
+            ( $env:Path -split ';' ) `
+            + $ToPath `
+            | Sort-Object -Unique `
+        ) `
+        -join ';' `
+    ;
+    [System.Environment]::SetEnvironmentVariable( 'PATH', $env:Path, [System.EnvironmentVariableTarget]::User );
+};
