83412549e7077ceeb0ef19a05d2efff870d55ad7
Sergey S. Betke <sergey.s.betke@yandex.ru>
2016-06-21 23:47:55 +0300

    * временно отключил установку ActivePerl на appveyor (#189)

--- install.ps1
+++ install.ps1
@@ -136,7 +136,7 @@ if ($PSCmdLet.ShouldProcess('WIXDIR', 'Установить переменную
 $ToPath += $env:WIXDIR;
 #>
 
-$null = Install-Package -Name ActivePerl -ProviderName Chocolatey -Source chocolatey;
+#$null = Install-Package -Name ActivePerl -ProviderName Chocolatey -Source chocolatey;
 
 Write-Verbose 'Preparing ctanify and ctanupload TeX scripts...';
 Function Install-PackageMikTeX {
