03731677fe429f25d213a1e9dce35ac9660fe62a
Sergey S. Betke <sergey.s.betke@yandex.ru>
2016-07-26 10:20:56 +0300

    - исправлены ошибки в скриптах установки и удаления пакета chocolatey (#149) [ci skip]

--- setup/chocolatey/font.gost2.304-81.install/sources/font.gost2.304-81.install/chocolateyInstall.ps1
+++ setup/chocolatey/font.gost2.304-81.install/sources/font.gost2.304-81.install/chocolateyInstall.ps1
@@ -1,7 +1,7 @@
 ﻿Install-ChocolateyInstallPackage `
     -packageName $env:chocolateyPackageName `
     -installerType msi `
-    -silentArgs '/qn /norestart' `
-    -filePath ( ( Get-Item '*.msi' )[0].Name ) `
-    -validExitCodes = @(0, 3010, 1641) `
+    -silentArgs '/passive /norestart' `
+    -file ( ( Get-Item -Path ( Join-Path -Path ( Split-Path -Parent $PSCommandPath ) -ChildPath '*.msi' ) )[0].FullName ) `
+    -validExitCodes @(0, 3010, 1641) `
 ;
