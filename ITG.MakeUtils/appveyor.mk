ae891f62a598155c0dd84c923de0960f84f84eef
Sergey S. Betke <sergey.s.betke@yandex.ru>
2016-07-27 03:10:35 +0300

    - добавлено вычисление длительности тестов (#149)

--- ITG.MakeUtils/appveyor.mk
+++ ITG.MakeUtils/appveyor.mk
@@ -18,15 +18,18 @@ testPlatformWrapper = \
   $(APPVEYORTOOL) AddTest -Name "$1" -Framework "MSTest" -FileName "" -Outcome Running; \
   STD_OUT_FILE=$$$$(mktemp); \
   STD_ERR_FILE=$$$$(mktemp); \
+  START_TIME=$$$$(($$$$(date +%s%3N))); \
   $2 > $$$$STD_OUT_FILE 2> $$$$STD_ERR_FILE; \
   EXIT_CODE=$$$$?; \
+  FINISH_TIME=$$$$(($$$$(date +%s%3N))); \
+  DURATION=$$$$(($$$$FINISH_TIME-$$$$START_TIME)); \
   STD_OUT="$$$$(cat $$$$STD_OUT_FILE)"; \
   STD_ERR="$$$$(cat $$$$STD_ERR_FILE)"; \
   echo $$$$STD_OUT; \
   if [[ $$$$EXIT_CODE -eq 0 ]]; then \
-    $(APPVEYORTOOL) UpdateTest -Name "$1" -Duration 1000 -Framework "MSTest" -FileName "" -Outcome Passed -StdOut $$$$STD_OUT; \
+    $(APPVEYORTOOL) UpdateTest -Name "$1" -Duration $$$$DURATION -Framework "MSTest" -FileName "" -Outcome Passed -StdOut $$$$STD_OUT; \
   else \
-    $(APPVEYORTOOL) UpdateTest -Name "$1" -Duration 1000 -Framework "MSTest" -FileName "" -Outcome Failed -StdOut $$$$STD_OUT -StdErr $$$$STD_ERR; \
+    $(APPVEYORTOOL) UpdateTest -Name "$1" -Duration $$$$DURATION -Framework "MSTest" -FileName "" -Outcome Failed -StdOut $$$$STD_OUT -StdErr $$$$STD_ERR; \
   fi; \
   exit $$$$EXIT_CODE;
 
