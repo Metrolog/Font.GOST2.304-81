9f9b1fcf633c2ce4f8a3a391c01052ca375bb870
Sergey S. Betke <sergey.s.betke@yandex.ru>
2016-08-02 00:10:30 +0300

    * добавлена возможность использования наиболее свежих версий nuget пакетов при сборке [ci skip]

--- ITG.MakeUtils/nuget.mk
+++ ITG.MakeUtils/nuget.mk
@@ -5,24 +5,16 @@ ITG_MAKEUTILS_DIR ?= $(MAKE_NUGET_DIR)
 include $(realpath $(ITG_MAKEUTILS_DIR)/common.mk)
 
 NUGET ?= nuget
+NUGET_PACKAGES_DIR ?= packages
 
-NUGET_PACKAGES_CONFIG  ?= packages.config
-NUGET_PACKAGES_DIR     ?= packages
-
-# $(call defineNugetPackagesConfig,ConfigFile,PackagesDir)
-define defineNugetPackagesConfig
-
-$(2)/%: $(1)
+$(NUGET_PACKAGES_DIR)/%: $(MAKEFILES)
 	$(NUGET) \
-    install $$(call winPath,$$<) \
-    -OutputDirectory $$(call winPath,$(2)) \
+    install $(firstword $(subst /,$(SPACE),$(patsubst $(NUGET_PACKAGES_DIR)/%,%,$@))) \
+    -OutputDirectory $(call winPath,$(NUGET_PACKAGES_DIR)) \
+    $(NUGET_PACKAGE_INSTALL_ARGS_$(firstword $(subst /,$(SPACE),$(patsubst $(NUGET_PACKAGES_DIR)/%,%,$@)))) \
     -ExcludeVersion
 
 clean::
-	rm -rf $(2)
-
-endef
-
-$(eval $(call defineNugetPackagesConfig,$(NUGET_PACKAGES_CONFIG),$(NUGET_PACKAGES_DIR)))
+	rm -rf $(NUGET_PACKAGES_DIR)
 
 endif
