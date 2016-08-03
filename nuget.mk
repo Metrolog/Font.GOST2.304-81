4f508c17090d4bb76fe2af9a6eeaa55f908063cd
Sergey S. Betke <sergey.s.betke@yandex.ru>
2016-08-03 10:35:27 +0400

    Hotfix/0.7.6 (#241)
    * разрешена публикация при сборке на AppVeyor после любого успешного комита в ветку master
    * устранены замечания по chocolatey пакету (по методу определения имени msi файла) (#235)
    * добавлены генерация .pvk и .spc файлов из .pfx, а также - экспорт .pfx (#236)
    + добавлена установка `signcode` в `install.ps1` (#236)
    * добавлен механизм выбора в `SIGNTARGET` инструмента подписи в зависимости от расширения имени файла (#236)
    + добавлен `SIGNCODE` для подписи шрифтов (#236)
    + добавлена установка `signcode-pwd.exe` и использован для пакетной подписи файлов с помощью `signcode` (#236)
    + добавлены тесты подписей ttf (`test-ttf`) (#236)
    + добавлен в репозиторий сертификат для подписи кода с паролем (#236)
    * обеспечено выполнение всех тестов (в случае ошибки в одном из них) (#238)
    * добавлена установка windows SDK для цифровых подписей (#210)
    * в pfx файле сертификата подписи добавлен сертификат центра сертификации для последующей установки в хранилище доверенных центров сертификации перед тестированием подписанных файлов (#236)
    + добавлено тестирование подписи msi (#236)
    + добавлен тест подписи msm (#236)
    + добавлены цифровые подписи в otf (#236, #109)

--- nuget.mk
+++ nuget.mk
@@ -5,15 +5,13 @@ ITG_MAKEUTILS_DIR ?= $(MAKE_NUGET_DIR)
 include $(realpath $(ITG_MAKEUTILS_DIR)/common.mk)
 
 NUGET ?= nuget
+NUGET_PACKAGES_DIR ?= packages
 
-NUGET_PACKAGES_CONFIG  ?= packages.config
-NUGET_PACKAGES_DIR     ?= packages
-
-$(NUGET_PACKAGES_DIR)/%: ;
-	$(info Expected package $@...)
+$(NUGET_PACKAGES_DIR)/%: $(MAKEFILES)
 	$(NUGET) \
-    install $(NUGET_PACKAGES_CONFIG) \
-    -OutputDirectory $(NUGET_PACKAGES_DIR) \
+    install $(firstword $(subst /,$(SPACE),$(patsubst $(NUGET_PACKAGES_DIR)/%,%,$@))) \
+    -OutputDirectory $(call winPath,$(NUGET_PACKAGES_DIR)) \
+    $(NUGET_PACKAGE_INSTALL_ARGS_$(firstword $(subst /,$(SPACE),$(patsubst $(NUGET_PACKAGES_DIR)/%,%,$@)))) \
     -ExcludeVersion
 
 clean::
