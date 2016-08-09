8b549af024abb117cd6f8c114a66664bbb2190b0
sergey.s.betke <sergey.s.betke@yandex.ru>
2016-08-09 19:20:05 +0300

    + добавлены шаблоны тестов установки и удаления для пакетов chocolatey

--- chocolatey.mk
+++ chocolatey.mk
@@ -3,6 +3,7 @@ MAKE_CHOCOLATEY_DIR := $(realpath $(dir $(lastword $(MAKEFILE_LIST))))
 ITG_MAKEUTILS_DIR   ?= $(MAKE_CHOCOLATEY_DIR)
 
 include $(realpath $(ITG_MAKEUTILS_DIR)/common.mk)
+include $(realpath $(ITG_MAKEUTILS_DIR)/tests.mk)
 
 CHOCO              ?= choco
 NUGET              ?= nuget
@@ -40,5 +41,53 @@ packChocoMSIPackage = $(call packChocoPackageAux,$1,$2,,$4,$5,$6)
 
 # $(call packChocoWebPackage, id, packageId, packageVersion, preReleaseSuffix, dependencies)
 packChocoWebPackage = $(call packChocoPackageAux,$1,$2,,$3,$4,$5)
+
+# test's templates for package
+
+# $(call defineInstallTestForChocoPackage,id,packageId)
+define defineInstallTestForChocoPackage
+
+$(call defineTest,install,$1,\
+  $(CHOCO) install $2 --force --confirm -pre --source "$$(<D)", \
+  $$($(1)TARGETS) \
+)
+
+endef
+
+# $(call defineInstallWithPowerShellTestForChocoPackage,id,packageId)
+define defineInstallWithPowerShellTestForChocoPackage
+
+$(call defineTest,install_with_powershell,$1,\
+  $(call psExecuteCommand,\
+    Set-Variable -Name ErrorActionPreference -Value Stop; \
+    Register-PackageSource -ProviderName Chocolatey -Name Test -Location '$$(call winPath,$$(abspath $$(<D)))' -Trusted -Verbose; \
+    Install-Package -Name $2 -Source Test -Force -Verbose; \
+    Unregister-PackageSource -Name Test -Verbose; \
+  ), \
+  $$($(1)TARGETS) \
+)
+
+endef
+
+# $(call defineUninstallTestForChocoPackage,id,packageId)
+define defineUninstallTestForChocoPackage
+
+$(call defineTest,uninstall,$1,\
+  $(CHOCO) uninstall $2 --confirm \
+)
+
+endef
+
+# $(call defineUninstallWithPowerShellTestForChocoPackage,id,packageId)
+define defineUninstallWithPowerShellTestForChocoPackage
+
+$(call defineTest,uninstall_with_powershell,$1,\
+  $(call psExecuteCommand,\
+    Set-Variable -Name ErrorActionPreference -Value Stop; \
+    Uninstall-Package -Name $2 -Verbose; \
+  ) \
+)
+
+endef
 
 endif
