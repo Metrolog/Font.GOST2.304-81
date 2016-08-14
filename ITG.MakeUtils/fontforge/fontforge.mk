e2194997583aef2e74d9075e26eaf3a1cf5beb99
Sergey S. Betke <sergey.s.betke@yandex.ru>
2016-08-14 17:49:34 +0300

    * добавлена заготовка fontforge.mk

---
+++ ITG.MakeUtils/fontforge/fontforge.mk
@@ -0,0 +1,30 @@
+ifndef MAKE_FONTFORGE_CTAN_DIR
+MAKE_FONTFORGE_CTAN_DIR := $(realpath $(dir $(lastword $(MAKEFILE_LIST))))
+ITG_MAKEUTILS_DIR ?= $(realpath $(MAKE_FONTFORGE_CTAN_DIR)/..)
+
+include $(ITG_MAKEUTILS_DIR)/common.mk
+include $(ITG_MAKEUTILS_DIR)/signing/sign.mk
+
+FONTFORGE          ?= fontforge \
+	-nosplash
+
+ifeq ($(OS),Windows_NT)
+PY                 ?= ffpython
+else
+PY                 ?= python
+endif
+
+AUTOHINT           ?= ttfautohint
+TTFAUTOHINT        ?= \
+  $(FONTSTOOLSDIR)/ttfautohint \
+    --hinting-range-min=8 \
+    --hinting-range-max=88 \
+    --hinting-limit=220 \
+    --increase-x-height=22 \
+    --windows-compatibility \
+    --composites \
+    --strong-stem-width="gGD" \
+    --no-info
+FASTFONT           ?= fastfont
+
+endif
